913f753c39d08d4c7cd9e7b617a7d199fd4e3b4c
unknown <lari.felt@edu.keuda.fi>
2023-09-01 10:59:01 +0300

    friday

---
+++ js/java1b.js
@@ -0,0 +1,29 @@
+﻿document.getElementById("kyselylomake").addEventListener("submit", function (event) {
+    var nimiKentta = document.getElementById("nimi");
+    var emailKentta = document.getElementById("email");
+    var ikaKentta = document.getElementById("ika");
+    var nimiVirhe = document.getElementById("nimiVirhe");
+    var emailVirhe = document.getElementById("emailVirhe");
+    var ikaVirhe = document.getElementById("ikaVirhe");
+
+    if (nimiKentta.validity.valueMissing || nimiKentta.validity.tooShort) {
+        nimiVirhe.style.display = "block";
+        event.preventDefault();
+    } else {
+        nimiVirhe.style.display = "none";
+    }
+
+    if (emailKentta.validity.valueMissing || emailKentta.validity.typeMismatch) {
+        emailVirhe.style.display = "block";
+        event.preventDefault();
+    } else {
+        emailVirhe.style.display = "none";
+    }
+
+    if (ikaKentta.validity.valueMissing || ikaKentta.validity.rangeUnderflow || ikaKentta.validity.rangeOverflow || isNaN(ikaKentta.value)) {
+        ikaVirhe.style.display = "block";
+        event.preventDefault();
+    } else {
+        ikaVirhe.style.display = "none";
+    }
+});
