5de1b0635a33fc9bca08c467c4341bad64b33e1e
unknown <lari.felt@edu.keuda.fi>
2023-11-17 09:49:39 +0200

    P7update

--- LastenKuvakirjaAanilla/js/elain.js
+++ LastenKuvakirjaAanilla/js/elain.js
@@ -1,6 +1,19 @@
-function elaimet(elain) {
-    aani = elain.id;
-    var audio = new Audio('sounds/' + aani + '.mp3');
-    audio.play();
+var nykyinenAani;
 
+function elaimet(elain) {
+    // Keskeyttää nykyisen ääni, jos sellainen on
+    if (nykyinenAani) {
+        nykyinenAani.pause();
+        nykyinenAani.currentTime = 0; 
+    }
+
+    // Hakee uuden äänen id:n ja luo Audio-objektin
+    var uusiAaniId = elain.id;
+    var uusiAani = new Audio('aanet/' + uusiAaniId + '.mp3');
+
+    // Tallentaa uuden äänen
+    nykyinenAani = uusiAani;
+
+    // Toistaa uuden äänen
+    uusiAani.play();
 }
