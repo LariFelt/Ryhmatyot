d29f34158f39fa95329e69382bb9ced28cda273a
electricitas <142588614+electricitas@users.noreply.github.com>
2023-12-07 12:06:12 +0200

    Add files via upload

---
+++ projekti6/js/paikallinenvarasto.js
@@ -0,0 +1,25 @@
+﻿document.getElementById("lomake").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var etunimi = document.getElementById("etunimi").value;
+    var sukunimi = document.getElementById("sukunimi").value;
+    var lahiosoite = document.getElementById("lahiosoite").value;
+    var postinumero = document.getElementById("postinumero").value;
+    var postitoimipaikka = document.getElementById("postitoimipaikka").value;
+    var puhelin = document.getElementById("puhelin").value;
+    var sahkoposti = document.getElementById("sahkoposti").value;
+
+    var tallennettavatTiedot = {
+        etunimi: etunimi,
+        sukunimi: sukunimi,
+        lahiosoite: lahiosoite,
+        postinumero: postinumero,
+        postitoimipaikka: postitoimipaikka,
+        puhelin: puhelin,
+        sahkoposti: sahkoposti
+    };
+
+    localStorage.setItem("kayttajanTiedot", JSON.stringify(tallennettavatTiedot));
+
+    alert("Tiedot tallennettu!");
+});
---
+++ projekti6/js/testi.js
@@ -0,0 +1,7 @@
+﻿let lause = "Maailmassa monta on ihmeellistä asiaa";
+
+let sana = /a+/g;
+
+let osumat = lause.match(sana);
+
+console.log(osumat);
